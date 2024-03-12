bbd4d094c9c5e532d0f7697a94336bf692ca1d65
napolly <aboutlim@gmail.com>
2024-03-12 19:59:40 +0900

    useEffect를 통하여 여러버튼에서 구현시 다른 함수에 영향을 미치는 예제

--- angma/voca/src/component/DayList.js
+++ angma/voca/src/component/DayList.js
@@ -5,8 +5,18 @@ export default function DayList() {
     const [days, setDays] = useState([]);
     const [count, setCount] = useState(0);
 
-    function onclick(){
-        setCount(count +1);
+    function onclick() {
+        setCount(count + 1);
+    }
+
+    function onclick2() {
+        setDays([
+            ...days,
+            {
+                id: Math.random(),
+                day: 1,
+            }
+        ])
     }
 
     useEffect(() => {
@@ -15,14 +25,16 @@ export default function DayList() {
 
     return (
         <>
-        <ul className="list_day">
-            {days.map(day => (
-                <li key={day.id}>
-                    <Link to={`day/${day.day}`}>Day {day.day}</Link>
-                </li>
-            ))}
-    </ul>
-    <button onClick={onclick}>{count}</button>
-    </>
+            <ul className="list_day">
+                {days.map(day => (
+                    <li key={day.id}>
+                        <Link to={`day/${day.day}`}>Day {day.day}</Link>
+                    </li>
+                ))}
+            </ul>
+            <button onClick={onclick}>{count}</button>
+            <button onClick={onclick2}>Day change</button>
+            {/* onclick2 버튼 클릭시 log에 보면 count도 계속 증가하고 있는것을 볼 수 있다. 버튼의count는 그대로인데 */}
+        </>
     );
 }
