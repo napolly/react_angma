af949362f48826fbb95aab56ceac3b3e257d8605
napolly <aboutlim@gmail.com>
2024-03-12 19:42:43 +0900

    12강, useEffect에 대한 첫번째 예제 버튼 onClick 이벤트로 count 증가 랜더링된 페이지에 상태값 변경

--- angma/voca/src/component/DayList.js
+++ angma/voca/src/component/DayList.js
@@ -1,14 +1,28 @@
-import dummy from "../db/data.json"
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom"
 
 export default function DayList() {
+    const [days, setDays] = useState([]);
+    const [count, setCount] = useState(0);
+
+    function onclick(){
+        setCount(count +1);
+    }
+
+    useEffect(() => {
+        console.log("Count change");
+    });
+
     return (
+        <>
         <ul className="list_day">
-            {dummy.days.map(day => (
+            {days.map(day => (
                 <li key={day.id}>
                     <Link to={`day/${day.day}`}>Day {day.day}</Link>
                 </li>
             ))}
     </ul>
+    <button onClick={onclick}>{count}</button>
+    </>
     );
 }
