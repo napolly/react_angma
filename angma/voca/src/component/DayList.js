74a065ddc6032b6ca342b66365d59e1871060ea4
napolly <aboutlim@gmail.com>
2024-03-12 20:23:03 +0900

    json-server를 이용하여 db/data.json의 데이터를 가져와 랜더링해줌. 즉, dummy 를 사용하지 않고 비동기 통신을 통하여 데이터 갱신 시점

--- angma/voca/src/component/DayList.js
+++ angma/voca/src/component/DayList.js
@@ -3,32 +3,23 @@ import { Link } from "react-router-dom"
 
 export default function DayList() {
     const [days, setDays] = useState([]);
-    const [count, setCount] = useState(0);
-
-    function onclick() {
-        setCount(count + 1);
-    }
-
-    function onclick2() {
-        setDays([
-            ...days,
-            {
-                id: Math.random(),
-                day: 1,
-            }
-        ])
-    }
 
     useEffect(() => {
-        console.log("Count change");
-    }, [count]);
+        //api 비동기 통신을 위해서 fetch를 이용
+        fetch('http://localhost:3001/days') //프라미스 반환?
+        .then(res =>{
+            return res.json();
+        })
+        .then(data => {
+            setDays(data);
+        });
+    }, []);
     //다른 함수에 영향을 미치는 것을 방지하기 위하여, useEffect의 두번째 매개변수에 배열을 전달해서.
     //이렇게 하면count가 변경됐을 때만 이 함수가 실행된다.
     //의존성 배열
     //의존성 배열의 값이 변경되는 경우에만 이 함수가 실행됨.
 
     return (
-        <>
             <ul className="list_day">
                 {days.map(day => (
                     <li key={day.id}>
@@ -36,9 +27,5 @@ export default function DayList() {
                     </li>
                 ))}
             </ul>
-            <button onClick={onclick}>{count}</button>
-            <button onClick={onclick2}>Day change</button>
-            {/* onclick2 버튼 클릭시 log에 보면 count도 계속 증가하고 있는것을 볼 수 있다. 버튼의count는 그대로인데 */}
-        </>
     );
 }
