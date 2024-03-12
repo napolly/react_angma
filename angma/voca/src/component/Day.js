ca01f4390a169e8e43ec376852a08fd52a733927
o0780 <aboutlim@gmail.com>
2024-03-12 16:26:11 +0900

    코딩앙마, 강의10, 라우터 구현에서 BrowserRouter, Switch, Route, Link 기능 완료

--- angma/voca/src/component/Day.js
+++ angma/voca/src/component/Day.js
@@ -1,17 +1,17 @@
 //특정날짜를 클릭했을 때 단어들이 나오는 페이지
 import dummy from "../db/data.json";
-// import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export default function Day() {
     // dummy.words 사용
-//   const { day } = useParams();
-const day=1;
-  const wordList = dummy.words.filter(word => word.day === day);
+  const { day } = useParams();
+  // const wordList = dummy.words.filter(word => word.day === day); 숫자와 문자를 비교하니 안됨
+  const wordList = dummy.words.filter(word => word.day === Number(day));
   console.log(wordList);
 
   return (
     <>
-      {/* <h2>Day {day}</h2> */}
+      <h2>Day {day}</h2>
       <table>
         <tbody>
           {wordList.map(word => (
