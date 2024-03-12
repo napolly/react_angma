68cdebede18f076e7b63d806b0c3b29ac70eec58
o0780 <aboutlim@gmail.com>
2024-03-12 17:00:21 +0900

    checkbox toggle 기능 추가 완료

--- angma/voca/src/component/Word.js
+++ angma/voca/src/component/Word.js
@@ -2,24 +2,31 @@ import { useState } from "react";
 
 export default function Word({ word }) {
     const [isShow, setIsShow] = useState(false); //초기값은 false : 그래야 단어를 외웠는지 안외웠는지
+    const [isDone, setIsDone] = useState(word.isDone);
 
     function toggleShow() {
         setIsShow(!isShow);
     }
 
+    function toggleDone() {
+        setIsDone(!isDone);
+    }
+
     return (
-        <tr>
+        <tr className={isDone ? 'off' : ''}>
               <td>
-                <input type="checkbox" />
+                <input type="checkbox" checked={isDone} 
+                onChange={toggleDone}
+                />
               </td>
               <td>{word.eng}</td>
               <td>{isShow && word.kor}</td> 
               {/*  isShow : True 일때만 뜻보여줌 */}
               <td>
                 <button onClick={toggleShow}>
-                    뜻 {isShow ? '숨기기':'보이기'}
+                    뜻 {isShow ? '숨기기':'보기'}
                     </button>
-                <button class="btn_del">삭제</button>
+                <button className="btn_del">삭제</button>
               </td>
             </tr>
     );
