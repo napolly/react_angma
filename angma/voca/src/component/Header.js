2dbddccdaffc25772b2e4e872d02c49d2372c5a0
o0780 <aboutlim@gmail.com>
2024-03-13 09:38:15 +0900

    day 안에서 단어추가, day 추가 선택시 페이지 저장폼 나타나지 않던 버그 수정 <Link to="create_word" className="link"> modify to <Link to="/create_word" className="link">

--- angma/voca/src/component/Header.js
+++ angma/voca/src/component/Header.js
@@ -7,10 +7,10 @@ export default function Header() {
                 <Link to="/">토익 영단어(고급)</Link>
             </h1>
             <div className="menu">
-                <Link to="create_word" className="link">
+                <Link to="/create_word" className="link">
                     단어 추가
                 </Link>
-                <Link to="create_day" className="link">
+                <Link to="/create_day" className="link">
                     Day 추가
                 </Link>
             </div>
